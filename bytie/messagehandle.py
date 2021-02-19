e1ffbc3355e66905d76609b5c980f2c51d3f677a
Ersagun Kuruca <ersagunkuruca@gmail.com>
2021-02-20 01:12:22 +0300

    Add 8ball command

--- bytie/messagehandle.py
+++ bytie/messagehandle.py
@@ -1,5 +1,5 @@
 import ast
-
+import hashlib
 
 def bytie_handle_hey_bytie() -> str:
     return "Yes, sir!"
@@ -13,3 +13,30 @@ def bytie_handle_ast(command: str) -> str:
 
 def bytie_handle_latex(command: str) -> str:
     return "https://latex.codecogs.com/png.latex?" + command
+
+
+eight_ball_messages = [
+    'As I see it, yes.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    'Don’t count on it.',
+    'It is certain.',
+    'It is decidedly so.',
+    'Most likely.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Outlook good.',
+    'Reply hazy, try again.',
+    'Signs point to yes.',
+    'Very doubtful.',
+    'Without a doubt.',
+    'Yes.',
+    'Yes – definitely.',
+    'You may rely on it.'
+]
+def bytie_handle_8ball(command: str) -> str:
+    number = int(hashlib.sha1(command.encode("utf-8")).hexdigest(), 16)
+    return eight_ball_messages[number % len(eight_ball_messages)]
