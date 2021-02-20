f3d501bb009597ee69172c9463b3fbb74b650f74
Yigit Ozkavci <yigitozkavci8@gmail.com>
2021-02-20 10:21:34 +0000

    Make bytie capable of generating top quality jokes.

--- tests/test_messagehandle.py
+++ tests/test_messagehandle.py
@@ -1,8 +1,8 @@
 from unittest import TestCase, main
+from unittest.mock import patch
 import bytie.messagehandle
 
 import ast
-
 
 class TestMessageHandler(TestCase):
 
@@ -15,6 +15,21 @@ class TestMessageHandler(TestCase):
         self.assertEqual(
             bytie.messagehandle.bytie_handle_ast(cmd),
             "Module(body=[Expr(value=Constant(value=4))], type_ignores=[])")
+
+    @patch('requests.get')
+    def test_dadjoke_success(self, mock_requests_get):
+        mock_requests_get.return_value.status_code = 200
+        mock_requests_get.return_value.json.return_value = {'id': '12345678', 'joke': 'an absolute unit of a joke'}
+        self.assertEqual(
+            bytie.messagehandle.bytie_handle_dadjoke(),
+            "https://icanhazdadjoke.com/j/12345678.png")
+
+    @patch('requests.get')
+    def test_dadjoke_fail(self, mock_requests_get):
+        mock_requests_get.return_value.status_code = 400
+        self.assertEqual(
+            bytie.messagehandle.bytie_handle_dadjoke(),
+            "Couldn't get a dadjoke :(")
 
 
 if __name__ == '__main__':
